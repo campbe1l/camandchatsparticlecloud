31235447660ec007412f920c1ba28c81786aa96e
campbe1l <campbell.cronk@cooper.edu>
2025-10-01 23:22:23 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import * as THREE from 'https://unpkg.com/three@0.152.2/build/three.module.js';
 import { OrbitControls } from 'https://unpkg.com/three@0.152.2/examples/jsm/controls/OrbitControls.js';
 import { OBJLoader } from 'https://unpkg.com/three@0.152.2/examples/jsm/loaders/OBJLoader.js';
 
+
 const canvas = document.getElementById('c');
 const renderer = new THREE.WebGLRenderer({ canvas, antialias: true, alpha: false });
 renderer.setPixelRatio(window.devicePixelRatio ? window.devicePixelRatio : 1);
